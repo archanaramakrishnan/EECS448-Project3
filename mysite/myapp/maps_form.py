5781b9d3c1a6b922ff62b1c560c60e77ea7be109
archanaramakrishnan <archana2359@gmail.com>
2019-10-30 20:11:01 -0500

    4 drop down menus and submit button. output logs to console

--- mysite/myapp/maps_form.py
+++ mysite/myapp/maps_form.py
@@ -1,11 +1,30 @@
 from django import forms
 
 class mapsForm(forms.Form):
-    building = forms.ChoiceField(choices=[('leep', 'LEEP2'),
+    building1 = forms.ChoiceField(choices=[('leep', 'LEEP2'),
                                         ('eaton', 'Eaton Hall'),
                                         ('learned', 'Learned Hall'),
                                         ('slawson', 'Slawson Hall'),
                                         ('budig', 'Budig Hall')
                                        ])
-    
 
+    building2 = forms.ChoiceField(choices=[('leep', 'LEEP2'),
+                                        ('eaton', 'Eaton Hall'),
+                                        ('learned', 'Learned Hall'),
+                                        ('slawson', 'Slawson Hall'),
+                                        ('budig', 'Budig Hall')
+                                       ])
+
+    building3 = forms.ChoiceField(choices=[('leep', 'LEEP2'),
+                                        ('eaton', 'Eaton Hall'),
+                                        ('learned', 'Learned Hall'),
+                                        ('slawson', 'Slawson Hall'),
+                                        ('budig', 'Budig Hall')
+                                       ])
+
+    building4 = forms.ChoiceField(choices=[('leep', 'LEEP2'),
+                                        ('eaton', 'Eaton Hall'),
+                                        ('learned', 'Learned Hall'),
+                                        ('slawson', 'Slawson Hall'),
+                                        ('budig', 'Budig Hall')
+                                       ])
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -20,5 +20,15 @@ def ratings_view(request):
     return render_to_response('ratings_view.html')
 
 def maps(request):
+    if request.method == "POST":
+        form = mapsForm(request.POST)
+        if form.is_valid():
+            building1=form.cleaned_data['building1']
+            building2=form.cleaned_data['building2']
+            building3=form.cleaned_data['building3']
+            building4=form.cleaned_data['building4']
+
+            print(building1, building2, building3, building4)
+
     form = mapsForm()
     return render(request, 'maps_form.html', {'form':form})
