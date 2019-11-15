1bca36c5835f140c71ef76fed0ed697fd5367977
archanaramakrishnan <archana2359@gmail.com>
2019-11-14 18:37:42 -0600

    distances calculated using haversine and printed to console

--- mysite/maps/urls.py
+++ mysite/maps/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     #url(r'^add/maps/distance.html', views.distance_calculator, name='distance_calculator'),
     #url(r'^add/maps/$', views.form_view, name='add_map'),
     url(r'^add/maps/$', views.add_map, name='add_map'),
-    url(r'^maps/(?P<id>\d+)/$', views.map, name='map'),
+    url(r'^maps/(?P<id>\d+)/$', views.distance_output, name='distance_output'),
 ]
--- mysite/maps/views.py
+++ mysite/maps/views.py
@@ -12,6 +12,27 @@ def add_map(request, id=id):
             #creates and saves an object bound to the form
             map_item=form.save(commit=False)
             map_item.save()
+
+            user_buildings=[]
+            user_buildings.append(Building.objects.get(name=form.cleaned_data['building1']))
+            user_buildings.append(Building.objects.get(name=form.cleaned_data['building2']))
+            user_buildings.append(Building.objects.get(name=form.cleaned_data['building3']))
+            user_buildings.append(Building.objects.get(name=form.cleaned_data['building4']))
+            user_buildings.append(Building.objects.get(name=form.cleaned_data['building5']))
+
+            bldg1 = Building.objects.get(name=form.cleaned_data['building1'])
+            print(bldg1.latitude)
+
+            print(user_buildings)
+            
+            distances = []
+            for i in range(0, len(user_buildings)-1):
+                distances.append(haversine((user_buildings[i].latitude, user_buildings[i].longitude), (user_buildings[i+1].latitude, user_buildings[i+1].longitude)))
+
+            print(distances)
+            
+            #context = { 'distances': distances }
+
             return redirect('/maps/' + str(map_item.id) + '/')
     else:
         form = MapForm()
@@ -19,16 +40,16 @@ def add_map(request, id=id):
     return render(request, 'maps/map_form.html', {'form':form})  
 
 
-def map(request, id=id):
-    #map=Map.objects.get(id=id)
-
+def distance_output(request, id=id):
     context = {
         'map':Map.objects.get(id=id),
-        'all_buildings': Building.objects.all()
+        'all_buildings': Building.objects.all(),
     }
 
     return render(request, 'maps/map.html', context=context)
 
+def shortest_distance(lat1, long1, lat2, long2):
+    return haversine((lat1, long1), (lat1, lat2))
 
     
 """
