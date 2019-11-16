9694f2501e78550d1a01b48094bece47dcc55c7f
archanaramakrishnan <archana2359@gmail.com>
2019-11-16 16:36:28 -0600

    displays time taken to walk

--- mysite/maps/views.py
+++ mysite/maps/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
-#from .map_form import MapForm
 from .models import Building, MapForm, Map
 from haversine import haversine, Unit
-
-# Create your views here.
 
 def add_map(request, id=id):
     if request.method == "POST":
@@ -19,40 +16,61 @@ def add_map(request, id=id):
             user_buildings.append(Building.objects.get(name=form.cleaned_data['building3']))
             user_buildings.append(Building.objects.get(name=form.cleaned_data['building4']))
             user_buildings.append(Building.objects.get(name=form.cleaned_data['building5']))
-
-            bldg1 = Building.objects.get(name=form.cleaned_data['building1'])
-            print(bldg1.latitude)
-
-            print(user_buildings)
             
             global global_distances
-            distances = []
+            global_distances = []
             for i in range(0, len(user_buildings)-1):
                 b1=user_buildings[i]
                 b2=user_buildings[i+1]
-                distances.append(round(haversine((b1.latitude, b1.longitude), (b2.latitude, b2.longitude)), 4))
+                global_distances.append(round(haversine((b1.latitude, b1.longitude), (b2.latitude, b2.longitude), unit=Unit.MILES), 4))
 
-            global_distances = distances[:]
+            distances = global_distances[:]
+
             print(distances)
+
+            global global_times
+            global_times = []
+
+            #average walking speed in miles/minutes of people in their 20s
+            avg_speed = 0.05033333
+
+            for distance in distances:
+                global_times.append(round(distance/avg_speed, 4))
+
+            #print(global_times)
 
             return redirect('/maps/' + str(map_item.id) + '/')
     else:
         form = MapForm()
 
     return render(request, 'maps/map_form.html', {'form':form})  
+"""
+def shortest_distance(lat1, long1, lat2, long2):
+    return haversine((lat1, long1), (lat1, lat2), unit=Unit.MILES)
 
+def walking_time():
+    distances = global_distances[:]
 
+    global times
+    times = []
+
+    #average walking speed in miles/minutes of people in their 20s
+    avg_speed = 0.05033333
+
+    for distance in distances:
+        times.append(distance/avg_speed)
+
+    print(times)
+"""
 def distance_output(request, id=id):
     context = {
         'map':Map.objects.get(id=id),
         'all_buildings': Building.objects.all(),
         'distances':global_distances,
+        'walking_times':global_times
     }
 
+    print(global_distances)
+    print(global_times)
+
     return render(request, 'maps/map.html', context=context)
-
-def shortest_distance(lat1, long1, lat2, long2):
-    return haversine((lat1, long1), (lat1, lat2), unit=Unit.MILES)
-
-def shortest_distance(lat1, long1, lat2, long2):
-    return haversine((lat1, long1), (lat1, lat2), unit=Unit.MILES)
