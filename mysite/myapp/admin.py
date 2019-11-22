e6a470c1b33f9fc9a3df18f1b6b442b292d0c874
Victoria Maldonado <vica.chevchenco@gmail.com>
2019-11-22 16:10:45 -0600

    Revert "trying to fix devel. leaving out pycache and migrations"
    This reverts commit 56f60aedc843debfe90127f4c5a5d6059455d140.

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
-from .models import Map
 # Register your models here.
-
-admin.site.register(Map)
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -109,9 +109,3 @@ class Rate(models.Model):
         max_length=20,
         choices=CLASS_OVERALL,
         default='I am so sorry for you',)
-# Create your models here.
-class Map(models.Model):
-    title = models.CharField(max_length=255)
-
-    def __str__(self):
-        return '%s' % self.title
