fa889404a12952495755d3ff1039d5b749503dfb
Sindhu Shakamuri <sindhu.shakamuri@gmail.com>
2019-11-22 18:01:12 -0600

    timer displayed on start of a button clicked

--- mysite/mysite/static/for_timer/js/refreshTime.js
+++ mysite/mysite/static/for_timer/js/refreshTime.js
@@ -1,15 +1,26 @@
-function refreshTime() {
-  $.ajax({
-    url: '{% url time %}',
-    success: function (data) {
-      console.log("success function called");
-      $('#test').html(data);
-    }
-  });
+
+var start = document.getElementById("starttimer");
+var dis = document.getElementById("countdown");
+var previousTime;
+//var timerLength = 30;
+var timeoutID;
+//dis.innerHTML = "Time Left: " + timerLength;
+
+if(localStorage.getItem('myTime')){
+    Update();
 }
 
+start.onclick = function () {
+    localStorage.setItem('myTime', ((new Date()).getTime()));
+    if (timeoutID != undefined) window.clearTimeout(timeoutID);
+    setInterval(Update, 1000);
+    Update();
+}
 
-$( document ).ready(function () {
-  console.log("setting interval");
-      setTimeout(refreshTime,1000);
-});
+function Update() {
+
+    previousTime = localStorage.getItem('myTime');
+    var timeStart = (new Date()- previousTime)/1000; //this will give
+    dis.innerHTML =   timeStart + " seconds";
+    timeoutID = window.setTimeout(Update, 100);
+}
