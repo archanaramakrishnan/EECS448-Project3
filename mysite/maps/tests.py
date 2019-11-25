f4254b63e9ac7e068613b392a2cfeb3b9febfe7a
archanaramakrishnan <archana2359@gmail.com>
2019-11-25 16:10:28 -0600

    added map tests

--- mysite/maps/tests.py
+++ mysite/maps/tests.py
@@ -1,15 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
-from maps.models import Building
+from maps.models import Building, Map
 
 class BuildingTestCase(TestCase):
     
-    def setUp(self):
+    def setUpMaps(self):
         Building.objects.create(name="Budig Hall", latitude="38.958312", longitude="-95.249133")
         Building.objects.create(name="Wescoe Hall", latitude="38.957712", longitude="-95.247629")
         Building.objects.create(name="LEEP2", latitude="38.957726", longitude="-95.254068")
 
+        Map.objects.create(building1="LEEP2", building2="Wescoe")
+
+    def test_created_maps_form(self):
+        """
+        Method `__str__` should be equal to field `name` and field `latitude` and `longitude` should be set correctly
+        """
+        f1 = Map.objects.get(building1="LEEP2")
+        self.assertEqual(str(f1), f1.building1)
+        print("PASSED: 'LEEP2' form created")
+    
     def test_created_buildings(self):
         """
         Method `__str__` should be equal to field `name` and field `latitude` and `longitude` should be set correctly
