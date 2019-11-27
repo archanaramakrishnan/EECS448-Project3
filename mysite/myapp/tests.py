d5cce775d1fe6a7b06f61dc8e7b3ad4d2973a8e2
archanaramakrishnan <archana2359@gmail.com>
2019-11-26 19:48:32 -0600

    Post class tests added

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -6,18 +6,17 @@ from myapp.models import Post
 class PostTestCase(TestCase):
 
     def setUp(self):
-        Post.objects.create(user_name='John Doe', title='Hi', text='Hi there')
-        Post.objects.create(user_name='Jane Doe', title='Bye', text='Goodbye')
-
-
+        Post.objects.create(user_name='John Doe', title='What motivates you to keep going', text='Talking to upper classmen, talking to my family')
+        Post.objects.create(user_name='Jane Doe', title='Best part about EECS 268', text='You get to learn A LOT. But no sleep!!')
 
     def test_created_post_form(self):
         """
         Method `__str__` should be equal to title field
         """
         print("\n")
-        f1 = Post.objects.get(title='Hi')
+        f1 = Post.objects.get(title='What motivates you to keep going')
         self.assertEqual(str(f1), f1.title)
-        f2 = Post.objects.get(title='Bye')
+        f2 = Post.objects.get(title='Best part about EECS 268')
         self.assertEqual(str(f2), f2.title)
-        print("PASSED: Post object created with title 'Hi' and 'Bye'")
+        print("PASSED: Post object created with title 'What motivates you to keep going'")
+        print("PASSED: Post object created with title 'Best part about EECS 268'")
