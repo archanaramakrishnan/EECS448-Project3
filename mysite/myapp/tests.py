5f9c36726c100444f3e0bcb3e9dbf25ebb28d809
Victoria Maldonado <vica.chevchenco@gmail.com>
2019-11-26 19:24:07 -0600

    Tests for advice

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+from myapp.models import Post
+
+class PostTestCase(TestCase):
+
+    def setUp(self):
+        Post.objects.create(user_name='John Doe', title='Hi', text='Hi there')
+        Post.objects.create(user_name='Jane Doe', title='Bye', text='Goodbye')
+
+
+
+    def test_created_post_form(self):
+        """
+        Method `__str__` should be equal to title field
+        """
+        print("\n")
+        f1 = Post.objects.get(title='Hi')
+        self.assertEqual(str(f1), f1.title)
+        f2 = Post.objects.get(title='Bye')
+        self.assertEqual(str(f2), f2.title)
+        print("PASSED: Post object created with title 'Hi' and 'Bye'")
