50b1309652d50fece7ca26ccd15475631a9925f4
Victoria Maldonado <vica.chevchenco@gmail.com>
2019-11-26 20:55:29 -0600

    Added tests for ratings

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from myapp.models import Post
+from myapp.models import Rate
 
 class PostTestCase(TestCase):
 
@@ -20,3 +21,24 @@ class PostTestCase(TestCase):
         self.assertEqual(str(f2), f2.title)
         print("PASSED: Post object created with title 'What motivates you to keep going'")
         print("PASSED: Post object created with title 'Best part about EECS 268'")
+
+class RateTestCase(TestCase):
+    def setUp(self):
+        Rate.objects.create(class_exams_num = '2', class_hw = 'Very Easy', class_comments = 'I liked the class, it was not hard')
+        Rate.objects.create(class_exams_num = '5', class_hw = 'Hard to understand', class_comments = 'Pretty hard class but you learn a lot')
+        Rate.objects.create(class_exams_num = '3', class_hw = 'Average', class_comments = 'Normal class, I liked it and the exams were not bad')
+
+    def test_created_post_form(self):
+        """
+        Method `__str__` should be equal to title field
+        """
+        print("\n")
+        f1 = Rate.objects.get(class_comments='I liked the class, it was not hard')
+        self.assertEqual(str(f1), f1.class_comments)
+        f2 = Rate.objects.get(class_comments='Pretty hard class but you learn a lot')
+        self.assertEqual(str(f2), f2.class_comments)
+        f3 = Rate.objects.get(class_comments='Normal class, I liked it and the exams were not bad')
+        self.assertEqual(str(f3), f3.class_comments)
+        print("PASSED: Rate object created with comment 'I liked the class, it was not hard'")
+        print("PASSED: Rate object created with comment 'Pretty hard class but you learn a lot'")
+        print("PASSED: Rate object created with comment 'Normal class, I liked it and the exams were not bad'")
